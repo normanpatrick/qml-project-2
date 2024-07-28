85b312ead4b63c9fff2588e57796807dae8cc3fb
Nirmalendu Patra <nbp@lbl.gov>
2024-07-28 10:52:43 -0500

    task5-dataset: TIGDataAccess wip

--- files/task5-TIG-welding/src/misc.py
+++ files/task5-TIG-welding/src/misc.py
@@ -1,5 +1,6 @@
 """
 Author: Nirmalendu B Patra
+Copyright (C) Nirmalendu B Patra - All Rights Reserved
 """
 
 import os
@@ -9,49 +10,103 @@ import numpy as np
 from PIL import Image
 import skimage.transform
 
+class TIGDataAccess(object):
+    """
+    - Pre-process, save normalized, non-normalized all in npy format
+    - Should behave like a seamless cache
+    """
+    def __init__(self,
+                 dir_in,
+                 json_files,
+                 how_many_classes,
+                 dir_preprocess):
+        self.dir_in = dir_in
+        self.dir_preprocess = dir_preprocess
+        self.json_files = json_files
+        self.how_many_classes=how_many_classes
+        self.td = None
+
+    """
+    - pre-process all images
+    - save normalized, non-normalized all in npy format
+    - allow for various resolutions
+    - API should support sklearn.train_test_split
+    """
+    def setup(self,
+              resize,
+              normalize,
+              max_items_per_label=0):
+        if self.td is None:
+            self.td = TIGDataset(topdir=self.dir_in,
+                                 json_files=self.json_files,
+                                 how_many_classes=self.how_many_classes,
+                                 resize=resize,
+                                 normalize=normalize,
+                                 max_items_per_label=max_items_per_label)
+            total = 0
+            for label in range(self.how_many_classes):
+                for img in self.td.next_sample(label=label):
+                    total += 1
+            print(f"Total processed: {total}")
+
 class TIGDataset(object):
     def __init__(self,
                  topdir,
                  json_files,
+                 how_many_classes,
+                 max_items_per_label=0,
                  resize=None,
-                 how_many_classes=6,
-                 seed=101):
+                 normalize=True):
         self.topdir = os.path.expanduser(topdir)
         self.json_files = json_files
         self.how_many_classes = how_many_classes
         self.dataset = {k: [] for k in range(how_many_classes)}
         self.duplicates = []
         self.resize = resize
+        self.normalize = normalize
+        # if n > 0, then process only n items, otherwise all
+        self.max_items_per_label = max_items_per_label
         self._consolidate()
-        random.seed(seed)
-        for label in self.dataset:
-            random.shuffle(self.dataset[label])
+        # random.seed(seed)
+        # for label in self.dataset:
+        #     random.shuffle(self.dataset[label])
 
     def _consolidate(self):
         already_seen = set()
+        totals = {k:0 for k in range(self.how_many_classes)}
         for jf, jdir in self.json_files:
             # print(os.path.join(self.topdir, jf))
             with open(os.path.join(self.topdir, jf)) as f:
                 jdata = json.loads(f.read())
                 for img,label in jdata.items():
+                    if self.max_items_per_label == totals[label] \
+                       and self.max_items_per_label > 0:
+                        continue
                     img_path = os.path.join(self.topdir, jdir, img)
                     if img_path not in already_seen:
+                        totals[label] += 1
                         already_seen.add(img_path)
                         self.dataset[label].append(img_path)
                     else:
                         self.duplicates.append(img_path)
 
-    def samples(self, d_how_many, normalize=True):
+    def next_sample(self, label):
+        for item in self.dataset[label]:
+            yield item
+
+    def _read_single_image(self, img):
+        assert os.path.exists(img) == True
+        imdata = self._image(img)
+        if self.normalize:
+            imdata = self._image(img) / 255 # np.linalg.norm(imdata)
+        return imdata
+
+    def samples(self, d_how_many):
         res = {}
         for label, num in d_how_many.items():
             res[label] = []
             for img in self.dataset[label][:num]:
-                if os.path.exists(img):
-                    imdata = self._image(img)
-                    if normalize:
-                        imdata = self._image(img) / 255 # np.linalg.norm(imdata)
-                    res[label].append(imdata)
-            # res[label] = self.dataset[label][:num]
+                res[label].append(self._read_single_image(img))
         return res
 
     def _image(self, fname):
--- files/task5-TIG-welding/src/test.py
+++ files/task5-TIG-welding/src/test.py
@@ -2,18 +2,21 @@ import numpy as np
 import misc
 
 topdir_dataset = "~/1/dataset/al5083"
+topdir_preprocess = "~/1/dataset/preprocess"
 json_files = [
     ("train/train.json", "train"),
     ("test/test.json", "test")
 ]
 
-def test(rsize=None, details=True, normalize=True):
+def _helper_01(rsize=None, details=True, normalize=True):
     td = misc.TIGDataset(topdir=topdir_dataset,
                          resize=rsize,
+                         normalize=normalize,
+                         how_many_classes=6,
                          json_files=json_files)
     if details:
         print(td)
-    samples = td.samples({0:10, 1:11, 5:15}, normalize=normalize)
+    samples = td.samples({0:10, 1:11, 5:15})
 
     for k in samples:
         fst = samples[k][0]
@@ -24,13 +27,26 @@ def test(rsize=None, details=True, normalize=True):
               ", Min:", np.min(fst),
               ", Mean:", np.sum(fst)/fst.size)
 
-def main():
+def test_01():
     for nm in [False, True]:
         print(f"---------------------- normalize = {nm} ---------------------")
         print("==== No compression ====")
-        test(normalize=nm)
+        _helper_01(normalize=nm)
         print("==== With compression ====")
-        test((60,50), details=False, normalize=nm)
+        _helper_01((60,50), details=False, normalize=nm)
+
+def test_02():
+    rsize = (60,50)
+    normalize = True
+    da = misc.TIGDataAccess(dir_in=topdir_dataset,
+                            how_many_classes=6,
+                            dir_preprocess=topdir_preprocess,
+                            json_files=json_files)
+    da.setup(max_items_per_label=5, resize=rsize, normalize=normalize)
+
+def main():
+    test_01()
+    test_02()
 
 if __name__ == '__main__':
     main()
