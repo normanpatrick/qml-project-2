e0ffc59d5352dfc5ad60ee63273b77e0e69fc4fe
Nirmalendu Patra <nbp@lbl.gov>
2024-07-29 13:54:12 -0500

    task5-dataset: added argparse to pre_process.py

--- files/task5-TIG-welding/src/pre_process.py
+++ files/task5-TIG-welding/src/pre_process.py
@@ -1,14 +1,23 @@
 import misc
+import argparse
 
-def main():
+def main(args):
     topdir_dataset = "~/1/dataset/al5083"
     topdir_preprocess = "~/1/dataset/preprocess"
     json_files = [
         ("train/train.json", "train"),
         ("test/test.json", "test")
 ]
-    rsize = (60,50)
-    normalize = True
+    rsize = (args.row, args.col)
+    normalize = not args.skip_normalize
+
+    print("Parameters")
+    print(f"  + resize   : {rsize}")
+    print(f"  + normalize: {normalize}")
+    if not args.real_run:
+        print("Dry-run - exiting. Use --real-run")
+        return
+
     da = misc.TIGDataAccess(dir_in=topdir_dataset,
                             how_many_classes=6,
                             dir_preprocess=topdir_preprocess,
@@ -16,4 +25,10 @@ def main():
     da.setup(max_items_per_label=0, resize=rsize, normalize=normalize)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--row", type=int, default=60)
+    parser.add_argument("--col", type=int, default=50)
+    parser.add_argument("--skip-normalize", action='store_true')
+    parser.add_argument("--real-run", action='store_true')
+    args = parser.parse_args()
+    main(args)
